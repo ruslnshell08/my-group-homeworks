da28e7560a38e4fc8fa344d226cf46767479f1d4
ruslnshell08 <72970869+ruslnshell08@users.noreply.github.com>
2020-10-28 12:22:49 +0300

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,15 @@
+print("Enter your name")
+name=input()
+print("Enter you age")
+age=input()
+str="Hello my name is  " 
+str1="Im   "
+print(str+name+str1+age)
+
+about_me_fstring=(F"Hello my name is {name}   im   {age}   ")
+print(F"Hello my name is {name}   im   {age}   ")
+s=about_me_fstring
+lis_from_str=s
+about_me_fstring.islower
+print(about_me_fstring)
+print(type(lis_from_str))
