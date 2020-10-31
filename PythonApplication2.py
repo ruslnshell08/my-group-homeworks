8f1a7ba72d2a5f656e690517a3c2ed07db1672fa
ruslnshell08 <72970869+ruslnshell08@users.noreply.github.com>
2020-10-31 13:47:08 +0300

    Add files via upload

---
+++ PythonApplication2.py
@@ -0,0 +1,21 @@
+
+sample_dict = { 
+   "class_a":{ 
+      "student":{ 
+         "name":"Misha",
+         "marks":{ 
+            "math":90,
+            "history":85,"phisics":90
+         }
+      }
+   }
+}
+print(sample_dict)
+print(sample_dict["class_a"]["student"]["name"])
+print(sample_dict["class_a"]["student"]["marks"])
+
+
+
+sample_dict={"class_a":{"student2":{"name":"Kolia","marks":{"history":90,"phisics":70,"math":100}}}}
+print(sample_dict)
+print(sample_dict["class_a"]["student2"]["name"])
