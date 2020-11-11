dd0df8699c9436ccfc5ba35919026da4d4390fa2
ruslnshell08 <72970869+ruslnshell08@users.noreply.github.com>
2020-11-11 17:07:57 +0300

    v7

--- Shelazenco/PythonApplication5.py
+++ Shelazenco/PythonApplication5.py
@@ -1,41 +1,34 @@
+data=open("users.txt","a+")
+
+l="qwerty"
 p=123
-l1="qwerty"
-print("проходили ли ви регистрацию")
-l=input()
-if(l=="yes"):
-    print("Введите свой пароль")
-elif(l=="no"):
-    print("Желаете пройти регистрацию")
-    k=input()
-    if(k=="yes"):
-        print("Введите свой пароль")
-        pp=input()
-        print("Введите свой логин")
-        ll=input()
-    
+register_check=input("Проходили ли вы регистрацию    ")
+if register_check==("yes"):
+    login=input("Введите логин ")
+    if login==l:
+        password=int(input("Введите пароль"))
+        if password==p:
+            print("Вы вошли в систему")
     else:
-        print("Досвидания!")
-
-
-
-f=int(input())
-print("Введите свой логин")
-f2=input()
-
-if p==f:
-    if l1==f2:
-        print("Вы вошли в систему!!!")
-    else:
-        print("Неверный логин или пароль")
+          print("Неверный логин или пароль")
+       
+elif register_check==("no"):
+    register_check2=input("Желаете ли пройти")
+    if register_check2=="yes":
+        users_login=input("Введите свой логин ")
+        data.write(users_login)
+        users_pas=input("Введите свой пароль ")
+        data.write(users_pas)
+        print("Привет    "+users_login)
+    elif register_check2=="no":
+        print("До свидания")
 else:
-    print("Неверный логин или пароль")
+    print("Неверный ответ вводите только 'yes' или 'no'")
+
+data.close()
 
 
-aa=open("users.txt","w")
-aa.write("                   ")
-aa.close()
-aa=open("users.txt","r")
-print(aa.read())
+
 
 
     
